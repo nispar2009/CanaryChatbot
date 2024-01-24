aa0912f72c018b0bb0a8cb221d208e0c199920c6
nispar2009 <varun.parupalli@gmail.com>
2024-01-24 20:45:34 +0530

    Presenting... the Canary Chatbot!!!

---
+++ script.js
@@ -0,0 +1,23 @@
+ask = () => {
+    const q = getInput("q")
+    if (q != "+") {
+        setValue("ans", "")
+        let answers = []
+        for (const iterator of Object.keys(localStorage)) {
+            if (q.toLowerCase().includes(iterator)) {
+                answers.push(localStorage.getItem(iterator))
+            }
+        }
+        for (const iterator of answers) {
+            document.getElementById("ans").innerHTML += `${iterator} `
+        }
+
+        if (answers.length == 0) {
+            setValue("ans", "Sorry, there are no matches for your search")
+        }
+    } else {
+        const kw = prompt("Enter a keyword below.")
+        const ans = prompt("Enter the answer below.")
+        localStorage.setItem(kw, ans)
+    }
+}
